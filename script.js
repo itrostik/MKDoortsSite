e89252bd444edb89426939f9f36ee8c86cb8f113
Rostislav Sergeev <113761086+itrostik@users.noreply.github.com>
2023-05-05 10:50:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,99 @@
+import "../styles/style.scss"
+import svg from '../img/icon_call.svg'
+console.log(svg)
+
+const arrow = document.querySelector('.header-top__menu-arrow')
+const menuDoors = document.querySelector('.menu-doors')
+const menu = document.querySelector('.header-top__menu')
+const menuItem = document.querySelectorAll('.header-top__menu-item:not(.menu-doors)')
+
+menuDoors.addEventListener('click', () => {
+        if (arrow.style.transform === 'matrix(1, 0, 0, -1, 0, 0)') {
+            arrow.style.transform = 'matrix(1, 0, 0, 1, 0, 0)'
+            menu.classList.remove('--open')
+            for (let i = 0; i < menuItem.length; i++) {
+                // menuItem[i].style.display = 'none';
+            }
+        }
+        else {
+            arrow.style.transform = 'matrix(1, 0, 0, -1, 0, 0)'
+            for (let i = 0; i < menuItem.length; i++) {
+                menuItem[i].style.display = 'flex'
+            }
+            menu.classList.add('--open')
+        }
+    }
+)
+
+
+const arrowInfo = document.querySelector('.arrowInfo')
+const button = document.querySelector('.info-text-body')
+const storeBlocks = document.querySelector('.header-top__info-blocks')
+
+button.addEventListener('click', () => {
+    if (arrowInfo.style.transform === 'matrix(1, 0, 0, -1, 0, 0)') {
+        arrowInfo.style.transform = 'matrix(1, 0, 0, 1, 0, 0)'
+        storeBlocks.style.animation = '0.7s forwards close';
+    }
+    else {
+        arrowInfo.style.transform = 'matrix(1, 0, 0, -1, 0, 0)'
+        storeBlocks.classList.add('open');
+        storeBlocks.style.animation = '0.7s forwards open';
+    }
+})
+
+
+
+const arrowCompany = document.querySelector('.item-arrow')
+const arrowCompanyBlock = document.querySelector('.company-arrow')
+const blockCompany = document.querySelector('.list-item-company')
+
+
+arrowCompanyBlock.addEventListener('click', () => {
+    if (arrowCompany.style.transform === 'matrix(1, 0, 0, -1, 0, 0)') {
+        arrowCompany.style.transform = 'matrix(1, 0, 0, 1, 0, 0)'
+        blockCompany.style.animation = '0.2s forwards closeCompany'
+    }
+    else {
+        arrowCompany.style.transform = 'matrix(1, 0, 0, -1, 0, 0)'
+        blockCompany.style.display = 'flex'
+        blockCompany.classList.add('-open')
+        blockCompany.style.animation = '0.2s forwards openCompany'
+    }
+})
+
+
+
+
+const bottomMenu = document.querySelector('.header-bottom-menu')
+
+const bottom = bottomMenu.parentNode
+
+const buttonSearch = document.querySelector('.header-bottom-search')
+const buttonSearchInput = document.querySelector('.inputSearch')
+const buttonIconSearch = document.querySelector('.uil-search');
+const buttonIconCross = document.createElement('i');
+buttonIconCross.classList.add('uil')
+buttonIconCross.classList.add('uil-times')
+bottomMenu.style.display = 'block'
+buttonSearch.addEventListener('click', () => {
+    if (bottomMenu.style.display === 'block') {
+        if (arrowCompany.style.transform !== 'matrix(1, 0, 0, -1, 0, 0)') {
+            blockCompany.style.animation = 'none'
+        }
+        bottomMenu.style.display = 'none'
+        buttonSearch.removeChild(buttonIconSearch)
+        buttonSearch.append(buttonIconCross)
+        buttonSearchInput.style.display = 'block';
+    }
+    else{
+        bottomMenu.style.display = 'block'
+        buttonSearchInput.style.display = 'none';
+        buttonSearch.removeChild(buttonIconCross)
+        buttonSearch.append(buttonIconSearch)
+    }
+})
+
+// console.log(bottom.children[1] === bottomMenu)
+// console.log((bottomMenu.style.display === 'block'))
+// console.log(bottom)
